ee23731961522a813a6f14ba5a9e72b7f6efbc89
Zabrodin Konstantin <fragnatt@gmail.com>
2015-09-22 16:48:31 +0300

    refactor main

--- app/src/main/java/ru/bondar/russify/MainActivity.java
+++ app/src/main/java/ru/bondar/russify/MainActivity.java
@@ -3,6 +3,7 @@ package ru.bondar.russify;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -36,63 +37,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextInput = (EditText) findViewById(R.id.text_input);
-        mTextOutput = (TextView) findViewById(R.id.text_output);
-
-        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-
-
-        mTextOutput.setOnClickListener(v -> {
-            ClipData clipData = ClipData.newPlainText("Translation", mTextOutput.getText().toString());
-            clipboardManager.setPrimaryClip(clipData);
-            Toast.makeText(this, R.string.text_copied, Toast.LENGTH_SHORT).show();
-        });
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(SERVICE_ENDPOINT)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .build();
-
-        TranslateAPI service = retrofit.create(TranslateAPI.class);
-
-
-        RxTextView.textChanges(mTextInput)
-                .filter(t -> {
-                    Log.d("STRING", t.toString());
-                    return t.length() != 0;
-                })
-                .debounce(DELAY_TIME, TimeUnit.MILLISECONDS)
-                .observeOn(Schedulers.io())
-                .subscribe(s -> service.detectLanguage(ACCESS_KEY, s.toString())
-                                .flatMap(j -> {
-                                    String result = j.get("lang").getAsString();
-                                    if (result.equals("ru")) {
-                                        return service.getTranslate(ACCESS_KEY, s.toString(), "ru-en");
-                                    } else {
-                                        return service.getTranslate(ACCESS_KEY, s.toString(), result + "-ru");
-                                    }
-                                })
-                                .observeOn(AndroidSchedulers.mainThread())
-                                .subscribe(new Subscriber<JsonObject>() {
-                                    @Override
-                                    public void onCompleted() {
-
-                                    }
-
-                                    @Override
-                                    public void onError(Throwable e) {
-                                        e.printStackTrace();
-                                    }
-
-                                    @Override
-                                    public void onNext(JsonObject jsonObject) {
-                                        mTextOutput.setText(jsonObject.get("text")
-                                                .getAsJsonArray().get(0).getAsString());
-                                    }
-                                })
-                );
-
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+//            landScapeOrientationInit();
+        } else {
+            portaitScapeOrientationInit();
+        }
     }
 
     @Override
@@ -115,5 +64,73 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void landScapeOrientationInit() {
+        translatorMenuInit();
+
+    }
+
+    private void portaitScapeOrientationInit() {
+        translatorMenuInit();
+    }
+
+    private void translatorMenuInit() {
+        mTextInput = (EditText) findViewById(R.id.text_input);
+        mTextOutput = (TextView) findViewById(R.id.text_output);
+
+        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+
+        mTextOutput.setOnClickListener(v -> {
+            ClipData clipData = ClipData.newPlainText("Translation", mTextOutput.getText().toString());
+            clipboardManager.setPrimaryClip(clipData);
+            Toast.makeText(this, R.string.text_copied, Toast.LENGTH_SHORT).show();
+        });
+
+        Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(SERVICE_ENDPOINT)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            .build();
+
+        TranslateAPI service = retrofit.create(TranslateAPI.class);
+
+
+        RxTextView.textChanges(mTextInput)
+                .filter(t -> {
+                    Log.d("STRING", t.toString());
+                    return t.length() != 0;
+            })
+            .debounce(DELAY_TIME, TimeUnit.MILLISECONDS)
+            .observeOn(Schedulers.io())
+            .subscribe(s -> service.detectLanguage(ACCESS_KEY, s.toString())
+                .flatMap(j -> {
+                    String result = j.get("lang").getAsString();
+                    if (result.equals("ru")) {
+                        return service.getTranslate(ACCESS_KEY, s.toString(), "ru-en");
+                    } else {
+                        return service.getTranslate(ACCESS_KEY, s.toString(), result + "-ru");
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<JsonObject>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onNext(JsonObject jsonObject) {
+                        mTextOutput.setText(jsonObject.get("text")
+                                .getAsJsonArray().get(0).getAsString());
+                    }
+                })
+            );
     }
 }
