0ac60a9f3a33894cc62c4e0f71de0d83843f93b9
Andrey Bondar <Alanai1995@gmail.com>
2015-09-24 19:17:53 +0300

    design fixes

--- app/src/main/java/ru/bondar/russify/MainActivity.java
+++ app/src/main/java/ru/bondar/russify/MainActivity.java
@@ -1,9 +1,14 @@
 package ru.bondar.russify;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,13 +28,13 @@ import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity implements TranslationFragment.OnFragmentInteractionListener {
 
     private EditText mTextInput;
-    private TextView mTextOutput;
     private final int DELAY_TIME = 400;
     private final String ACCESS_KEY = "trnsl.1.1.20150911T085342Z.79f8b7b676e2face.b1fca036ecb27cb8260a55bf9704ff61a0e006b9";
     private final String SERVICE_ENDPOINT = "https://translate.yandex.net";
+    private FragmentManager fManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,15 +42,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         mTextInput = (EditText) findViewById(R.id.text_input);
-        mTextOutput = (TextView) findViewById(R.id.text_output);
 
         ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
 
-        mTextOutput.setOnClickListener(v -> {
-            ClipData clipData = ClipData.newPlainText("Translation", mTextOutput.getText().toString());
-            clipboardManager.setPrimaryClip(clipData);
-            Toast.makeText(this, R.string.text_copied, Toast.LENGTH_SHORT).show();
-        });
+        fManager = getFragmentManager();
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+//        mTextOutput.setOnClickListener(v -> {
+//            ClipData clipData = ClipData.newPlainText("Translation", mTextOutput.getText().toString());
+//            clipboardManager.setPrimaryClip(clipData);
+//            Toast.makeText(this, R.string.text_copied, Toast.LENGTH_SHORT).show();
+//        });
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(SERVICE_ENDPOINT)
@@ -54,6 +62,15 @@ public class MainActivity extends Activity {
                 .build();
 
         TranslateAPI service = retrofit.create(TranslateAPI.class);
+
+        TranslationFragment fragment = TranslationFragment
+                .newInstance("");
+
+        FragmentTransaction fTrans = fManager.beginTransaction();
+
+        fTrans.add(R.id.trn_fragment_container, fragment);
+        fTrans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fTrans.commit();
 
 
         RxTextView.textChanges(mTextInput)
@@ -86,8 +103,7 @@ public class MainActivity extends Activity {
 
                                     @Override
                                     public void onNext(JsonObject jsonObject) {
-                                        mTextOutput.setText(jsonObject.get("text")
-                                                .getAsJsonArray().get(0).getAsString());
+                                        fragment.setText(jsonObject.get("text").getAsJsonArray().get(0).getAsString());
                                     }
                                 })
                 );
@@ -114,5 +130,10 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
